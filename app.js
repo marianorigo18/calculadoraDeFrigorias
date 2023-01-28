8424e05d46bb2b1d5392e4c7858bba459c24fba1
marianorigo18 <marianowagnerrigo@gmail.com>
2023-01-27 21:57:34 -0300

    first commiot

---
+++ app.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let inputFrig = document.querySelector('#inputFrigorias').value
+    if(inputFrig == ""){
+        return console.log('No se permiten valores vacios');
+    }
+    if(!isNaN(inputFrig) == false){
+        return console.log('Se debe ingresar solo numeros')
+    }
+    calcularFrigorias(inputFrig);
+})
+
+function calcularFrigorias(frigorias){
+    let resultado = Math.floor(frigorias / 0.846 / 220 / 2)
+    console.log(resultado + "A")
+}

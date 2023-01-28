4fb03cfeb30660035fb846732a800e082be01222
marianorigo18 <marianowagnerrigo@gmail.com>
2023-01-28 10:41:53 -0300

    first commit

--- app.js
+++ app.js
@@ -7,17 +7,42 @@ form.addEventListener('submit', (e)=>{
     e.preventDefault();
     let inputFrig = document.querySelector('#inputFrigorias').value
     if(inputFrig == ""){
-        return console.log('No se permiten valores vacios');
+        return mostrarAlerta('No se permiten valores vacios', e.target);
     }
     if(!isNaN(inputFrig) == false){
-        return console.log('Se debe ingresar solo numeros')
+        return mostrarAlerta('Se debe ingresar solo numeros', e.target)
     }
-    calcularFrigorias(inputFrig);
+    calcularFrigorias(inputFrig, e.target);
     form.reset();
 })
 }
 
-function calcularFrigorias(frigorias){
+function calcularFrigorias(frigorias, referencia){
     let resultado = Math.floor(frigorias / 0.846 / 220 / 2)
-    console.log(resultado + "A")
+    mostrarMensaje(resultado, referencia);
+}
+
+function mostrarMensaje(result, referencia){
+    let mensaje = document.createElement('P');
+    mensaje.textContent = result;
+    mensaje.classList.add('mensaje');
+    referencia.appendChild(mensaje)
+}
+
+function mostrarAlerta(mensaje, referencia){
+    limpiarAlert(referencia)
+    let alert = document.createElement('P');
+    alert.textContent = mensaje;
+    alert.classList.add('alert');
+    referencia.appendChild(alert);
+}
+
+function limpiarAlert(referencia){
+    let alert = referencia.querySelector('.alert');
+    if(alert){
+        alert.remove();
+    }
+    setTimeout(function(){
+        alert.remove();
+    },2000)
 }
